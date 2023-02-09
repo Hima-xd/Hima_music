f322346995d27872d58dd98f4842a128d3ef89ad
Hima-xd <124744147+Hima-xd@users.noreply.github.com>
2023-02-09 07:33:28 +0530

    Update misc.py

--- Bikash/misc.py
+++ Bikash/misc.py
@@ -4,8 +4,8 @@ import time
 import heroku3
 from pyrogram import filters
 
-from Bikash import config
-from Bikash.core.mongo import pymongodb
+from Zaan import config
+from Zaan.core.mongo import pymongodb
 
 from .logging import LOGGER
 
