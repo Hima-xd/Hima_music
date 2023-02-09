8384737418bea10bca1c7c5cb94ce391c489bfbb
Hima-xd <124744147+Hima-xd@users.noreply.github.com>
2023-02-09 07:28:03 +0530

    Update call.py

--- Bikash/core/call.py
+++ Bikash/core/call.py
@@ -1,4 +1,4 @@
-# Powered By BikashHalder Or Aditya Halder IF You Fresh Any Problem To Contact The BgtRobot Owner
+# Powered By Zaan IF You Fresh Any Problem To Contact The BgtRobot Owner
 
 import asyncio
 from datetime import datetime, timedelta
